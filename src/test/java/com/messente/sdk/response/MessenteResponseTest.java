65e3bca3e353da58ca7b87e8b9a0f96a8ceb94c8
lkallas <lennar.kallas@messente.com>
2016-09-07 15:54:41 +0300

    Generate tests skeleton

---
+++ src/test/java/com/messente/sdk/response/MessenteResponseTest.java
@@ -0,0 +1,114 @@
+/*
+ * Copyright 2016 Messente Communications Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.messente.sdk.response;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Lennar Kallas
+ */
+public class MessenteResponseTest {
+
+    public MessenteResponseTest() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() {
+    }
+
+    @AfterClass
+    public static void tearDownClass() {
+    }
+
+    @Before
+    public void setUp() {
+    }
+
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of getRawResponse method, of class MessenteResponse.
+     */
+    @Test
+    public void testGetRawResponse() {
+
+    }
+
+    /**
+     * Test of getHttpResponseCode method, of class MessenteResponse.
+     */
+    @Test
+    public void testGetHttpResponseCode() {
+
+    }
+
+    /**
+     * Test of isSuccess method, of class MessenteResponse.
+     */
+    @Test
+    public void testIsSuccess() {
+
+    }
+
+    /**
+     * Test of getResult method, of class MessenteResponse.
+     */
+    @Test
+    public void testGetResult() {
+
+    }
+
+    /**
+     * Test of getContentAfterPrefix method, of class MessenteResponse.
+     */
+    @Test
+    public void testGetContentAfterPrefix() {
+
+    }
+
+    /**
+     * Test of getResponseMessage method, of class MessenteResponse.
+     */
+    @Test
+    public void testGetResponseMessage() {
+
+    }
+
+    /**
+     * Test of getDlrMessage method, of class MessenteResponse.
+     */
+    @Test
+    public void testGetDlrMessage() {
+
+    }
+
+    /**
+     * Test of getFailureMessage method, of class MessenteResponse.
+     */
+    @Test
+    public void testGetFailureMessage() {
+
+    }
+
+}
