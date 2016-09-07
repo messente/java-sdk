c0c2502e825c671d1a2df4acf91556c8a5b4d2b0
lkallas <lennar.kallas@messente.com>
2016-09-07 15:46:11 +0300

    Add unit tests for URL building methods.

--- src/test/java/com/messente/sdk/MessenteTest.java
+++ src/test/java/com/messente/sdk/MessenteTest.java
@@ -15,19 +15,58 @@
  */
 package com.messente.sdk;
 
+import com.messente.sdk.enums.Autoconvert;
+import com.messente.sdk.enums.Country;
+import com.messente.sdk.enums.HttpMethod;
+import com.messente.sdk.enums.HttpProtocol;
+import com.messente.sdk.enums.ResponseFormat;
+import com.messente.sdk.options.MessenteOptions;
+
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import static org.junit.Assert.*;
 
 /**
+ * Unit tests for Messente class. Mainly covers URL building methods.
  *
  * @author Lennar Kallas
  */
 public class MessenteTest {
 
+    private final String DUMMY_SENDER = "DummySender";
+    private final String DUMMY_RECIPIENT = "+3725123456";
+    private final String DUMMY_SMS = "This is a dummy SMS!";
+
+    private final String DUMMY_MSGID = "23i42o35hl3hh352";
+
+    private final String DUMMY_MESSENTE_USER = "dummy_messente_user";
+    private final String DUMMY_MESSENTE_PASSWORD = "dummy_messente_password";
+
+    private final String MESSENTE_API_SERVER = "api2.messente.com";
+    private final String MESSENTE_API_BACKUP_SERVER = "api3.messente.com";
+
+    private final Messente MESSENTE;
+
+    private final MessenteOptions MESSENTE_OPTIONS = new MessenteOptions.Builder()
+            .autoconvert(Autoconvert.ON) // Character replacement setting
+            .charset("UTF-8") // Encoding for SMS text and sender ID
+            .dlrUrl("http://www.yourdomain.com/process_dlr.php") // Delivery report URL
+            .httpMethod(HttpMethod.POST) // HTTP method that is used for API call
+            .protocol(HttpProtocol.HTTPS) // HTTP protocol used for API call
+            .udh(null) // UDH (User Data Header)
+            .validity("60") // For how long message is retried if phone is off (minutes)
+            .timeToSend("1453276295") // UNIX timestamp for delayed sending
+            .build();   // Finally build options
+
     public MessenteTest() {
+        this.MESSENTE = new Messente(
+                DUMMY_MESSENTE_USER,
+                DUMMY_MESSENTE_PASSWORD,
+                MESSENTE_API_SERVER,
+                MESSENTE_API_BACKUP_SERVER);
     }
 
     @BeforeClass
@@ -47,50 +86,30 @@ public class MessenteTest {
     }
 
     /**
-     * Test of getDeliveryStatus method, of class Messente.
+     * Test of getMessagingURL method, of class Messente.
      */
     @Test
-    public void testGetDeliveryStatus_String() throws Exception {
+    public void testGetMessagingURL_3args() {
 
-    }
+        String expectedUrl = "https://api2.messente.com/send_sms/?"
+                + "username=" + DUMMY_MESSENTE_USER
+                + "&password=" + DUMMY_MESSENTE_PASSWORD
+                + "&from=" + DUMMY_SENDER
+                + "&to=%2B3725123456"
+                + "&text=This+is+a+dummy+SMS%21"
+                + "&charset=UTF-8";
 
-    /**
-     * Test of getDeliveryStatus method, of class Messente.
-     */
-    @Test
-    public void testGetDeliveryStatus_String_MessenteOptions() throws Exception {
+        try {
+            String actualUrl = MESSENTE.getMessagingURL(
+                    DUMMY_SENDER,
+                    DUMMY_RECIPIENT,
+                    DUMMY_SMS)
+                    .toString();
 
-    }
-
-    /**
-     * Test of getPriceList method, of class Messente.
-     */
-    @Test
-    public void testGetPriceList_Country() throws Exception {
-
-    }
-
-    /**
-     * Test of getPriceList method, of class Messente.
-     */
-    @Test
-    public void testGetPriceList_Country_MessenteResponseFormat() throws Exception {
-
-    }
-
-    /**
-     * Test of getPriceList method, of class Messente.
-     */
-    @Test
-    public void testGetPriceList_Country_MessenteOptions() throws Exception {
-
-    }
-
-    /**
-     * Test of getPriceList method, of class Messente.
-     */
-    @Test
-    public void testGetPriceList_3args() throws Exception {
+            assertEquals(expectedUrl, actualUrl);
+        } catch (Exception e) {
+            fail(e.getMessage());
+        }
 
     }
 
@@ -98,7 +117,26 @@ public class MessenteTest {
      * Test of getMessagingURL method, of class Messente.
      */
     @Test
-    public void testGetMessagingURL_3args() throws Exception {
+    public void testGetMessagingURL_String_String() {
+
+        String expectedUrl = "https://api2.messente.com/send_sms/?"
+                + "username=" + DUMMY_MESSENTE_USER
+                + "&password=" + DUMMY_MESSENTE_PASSWORD
+                + "&to=%2B3725123456"
+                + "&text=This+is+a+dummy+SMS%21"
+                + "&charset=UTF-8";
+        try {
+
+            String actualUrl = MESSENTE.getMessagingURL(
+                    DUMMY_RECIPIENT,
+                    DUMMY_SMS)
+                    .toString();
+
+            assertEquals(expectedUrl, actualUrl);
+
+        } catch (Exception e) {
+            fail(e.getMessage());
+        }
 
     }
 
@@ -106,15 +144,34 @@ public class MessenteTest {
      * Test of getMessagingURL method, of class Messente.
      */
     @Test
-    public void testGetMessagingURL_String_String() throws Exception {
+    public void testGetMessagingURL_4args() {
 
-    }
+        String expectedUrl = "https://api2.messente.com/send_sms/?"
+                + "username=" + DUMMY_MESSENTE_USER
+                + "&password=" + DUMMY_MESSENTE_PASSWORD
+                + "&from=DummySender"
+                + "&to=%2B3725123456"
+                + "&text=This+is+a+dummy+SMS%21"
+                + "&charset=UTF-8"
+                + "&autoconvert=on"
+                + "&time_to_send=1453276295"
+                + "&dlr-url=http%3A%2F%2Fwww.yourdomain.com%2Fprocess_dlr.php"
+                + "&validity=60";
 
-    /**
-     * Test of getMessagingURL method, of class Messente.
-     */
-    @Test
-    public void testGetMessagingURL_4args() throws Exception {
+        try {
+
+            String actualUrl = MESSENTE.getMessagingURL(
+                    DUMMY_SENDER,
+                    DUMMY_RECIPIENT,
+                    DUMMY_SMS,
+                    MESSENTE_OPTIONS)
+                    .toString();
+
+            assertEquals(expectedUrl, actualUrl);
+
+        } catch (Exception e) {
+            fail(e.getMessage());
+        }
 
     }
 
@@ -122,8 +179,26 @@ public class MessenteTest {
      * Test of getPricingURL method, of class Messente.
      */
     @Test
-    public void testGetPricingURL_MessenteResponseFormat_Country() throws Exception {
+    public void testGetPricingURL_MessenteResponseFormat_Country() {
 
+        String expectedUrl = "https://api2.messente.com/prices/?"
+                + "username=" + DUMMY_MESSENTE_USER
+                + "&password=" + DUMMY_MESSENTE_PASSWORD
+                + "&country=HU"
+                + "&format=xml";
+
+        try {
+
+            String actualUrl = MESSENTE.getPricingURL(
+                    ResponseFormat.XML,
+                    Country.HUNGARY)
+                    .toString();
+
+            assertEquals(expectedUrl, actualUrl);
+
+        } catch (Exception e) {
+            fail(e.getMessage());
+        }
     }
 
     /**
@@ -132,13 +207,49 @@ public class MessenteTest {
     @Test
     public void testGetPricingURL_3args() throws Exception {
 
+        String expectedUrl = "https://api2.messente.com/prices/?"
+                + "username=" + DUMMY_MESSENTE_USER
+                + "&password=" + DUMMY_MESSENTE_PASSWORD
+                + "&country=HU"
+                + "&format=xml";
+
+        try {
+
+            String actualUrl = MESSENTE.getPricingURL(
+                    ResponseFormat.XML,
+                    Country.HUNGARY,
+                    MESSENTE_OPTIONS)
+                    .toString();
+
+            assertEquals(expectedUrl, actualUrl);
+
+        } catch (Exception e) {
+            fail(e.getMessage());
+        }
+
     }
 
     /**
      * Test of getDlrURL method, of class Messente.
      */
     @Test
-    public void testGetDlrURL_String() throws Exception {
+    public void testGetDlrURL_String() {
+
+        String expectedUrl = "https://api2.messente.com/get_dlr_response/?"
+                + "username=" + DUMMY_MESSENTE_USER
+                + "&password=" + DUMMY_MESSENTE_PASSWORD
+                + "&sms_unique_id=" + DUMMY_MSGID;
+
+        try {
+
+            String actualUrl = MESSENTE.getDlrURL(DUMMY_MSGID)
+                    .toString();
+
+            assertEquals(expectedUrl, actualUrl);
+
+        } catch (Exception e) {
+            fail(e.getMessage());
+        }
 
     }
 
@@ -146,7 +257,23 @@ public class MessenteTest {
      * Test of getDlrURL method, of class Messente.
      */
     @Test
-    public void testGetDlrURL_String_MessenteOptions() throws Exception {
+    public void testGetDlrURL_String_MessenteOptions() {
+
+        String expectedUrl = "https://api2.messente.com/get_dlr_response/?"
+                + "username=" + DUMMY_MESSENTE_USER
+                + "&password=" + DUMMY_MESSENTE_PASSWORD
+                + "&sms_unique_id=" + DUMMY_MSGID;
+
+        try {
+
+            String actualUrl = MESSENTE.getDlrURL(DUMMY_MSGID, MESSENTE_OPTIONS)
+                    .toString();
+
+            assertEquals(expectedUrl, actualUrl);
+
+        } catch (Exception e) {
+            fail(e.getMessage());
+        }
 
     }
 
@@ -154,24 +281,44 @@ public class MessenteTest {
      * Test of getCreditsURL method, of class Messente.
      */
     @Test
-    public void testGetCreditsURL_0args() throws Exception {
+    public void testGetCreditsURL_0args() {
 
+        String expectedUrl = "https://api2.messente.com/get_balance/?"
+                + "username=" + DUMMY_MESSENTE_USER
+                + "&password=" + DUMMY_MESSENTE_PASSWORD;
+
+        try {
+
+            String actualUrl = MESSENTE.getCreditsURL()
+                    .toString();
+
+            assertEquals(expectedUrl, actualUrl);
+
+        } catch (Exception e) {
+            fail(e.getMessage());
+        }
     }
 
     /**
      * Test of getCreditsURL method, of class Messente.
      */
     @Test
-    public void testGetCreditsURL_MessenteOptions() throws Exception {
+    public void testGetCreditsURL_MessenteOptions() {
 
-    }
+        String expectedUrl = "https://api2.messente.com/get_balance/?"
+                + "username=" + DUMMY_MESSENTE_USER
+                + "&password=" + DUMMY_MESSENTE_PASSWORD;
 
-    /**
-     * Test of getMyIP method, of class Messente.
-     */
-    @Test
-    public void testGetMyIP() throws Exception {
+        try {
 
+            String actualUrl = MESSENTE.getCreditsURL(MESSENTE_OPTIONS)
+                    .toString();
+
+            assertEquals(expectedUrl, actualUrl);
+
+        } catch (Exception e) {
+            fail(e.getMessage());
+        }
     }
 
 }
